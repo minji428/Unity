f2472cb91143598250941a820dd05312772b8881
minji428 <alswl980428@gmail.com>
2021-09-24 19:10:17 +0900

    swipecar2

---
+++ swipecar2/Assets/CarController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public GameObject flag;
+    float speed = 0;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = (endPos.x - this.startPos.x);
+
+            this.speed = swipeLength / 500.0f;
+            GetComponent<AudioSource>().Play();
+        }
+
+        // 깃발에 도달하면 차가 멈춤 
+        if (transform.position.x > flag.transform.position.x)
+        {
+            this.speed = 0;
+        }
+        else
+        {
+            transform.Translate(this.speed, 0, 0);
+            this.speed *= 0.98f;
+        }
+    }
+}
