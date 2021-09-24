61c8a5cc3766aa7da108f5084ebfc25db971368e
minji428 <alswl980428@gmail.com>
2021-09-24 19:12:07 +0900

    ClimbCloud

---
+++ ClimbCloud/Assets/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float jumpForce = 650.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+    int key = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    public void LButtonDown()
+    {
+        key = -1;
+    }
+    public void RButtonDown()
+    {
+        key = 1;
+
+    }
+    public void RButtonUp()
+    {
+        key = 0;
+    }
+    public void LButtonUp()
+    {
+        key = 0;
+    }
+
+
+    public void UButtonDown()
+    {
+        if (this.rigid2D.velocity.y == 0)
+        {
+            this.animator.SetTrigger("JumpTrigger");
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // 플레이어 속도
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        // 스피드 제한
+        if(speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        // 움직이는 방향에 따라 반전
+        if(key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        // 플레이어 속도에 맞춰 애니메이션 속도 바꿈
+        //this.animator.speed = speedx / 2.0f;
+        if(this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+
+        // 플레이어가 화면 밖으로 나갔다면 처음부터
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        // 벽면으로 갔을 때 떨어지지 않아야 함
+        if(transform.position.x > 3 && transform.position.x < -3)
+        {
+            
+        }
+    }
+    
+
+    // 골 도착
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("골");
+        SceneManager.LoadScene("ClearScene");
+    }
+}
